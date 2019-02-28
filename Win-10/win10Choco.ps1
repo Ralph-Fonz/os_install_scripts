3821de2e786b634382940b8e211835e74106b9b4
Ctrl_Null <raphael@fonseca.tech>
2019-02-28 09:57:56 -0500

    Updated install script for Windows to support various situations

--- Win-10/win10Choco.ps1
+++ Win-10/win10Choco.ps1
@@ -11,11 +11,59 @@ cinst -y GoogleChrome
 
 # Creative Programs
 cinst -y gimp
-cinst -y spotify
+cinst -y OpenOffice
 
+
+$reply = Read-Host -Prompt "Is his a Dev Machine?[y/n]"
+if ( $reply -match "[yY]" ) { 
 # Dev Tools
-cinst -y vscode
+  cinst -y vscode
+  cinst -y notepadplusplus
+  cinst -y slack
+  cinst -y discord
+  cinst -y insomnia-rest-api-client
+  cinst -y nodejs.install
+  cinst -y git.install
+$reply = Read-Host -Prompt "Is this for backend?[y/n]"
+  if ( $reply -match "[yY]" ) { 
+    cinst -y sql-server-management-studio
+    cinst -y visualstudio2017-installer
+  }
+}
+
+# VM || PenTesting
+
+$reply = Read-Host -Prompt "Will this be used for Pen testing or running VM's?[y/n]"
+if ( $reply -match "[yY]" ) { 
+  cinst -y virtualbox
+}
+
+# File Managment
+cinst -y filezilla
 cinst -y hyper
-cinst -y notepadplusplus
-cinst -y slack
-cinst -y insomnia-rest-api-client
+
+
+
+# Video Service
+cinst -y vlc
+
+# Gaming
+$reply = Read-Host -Prompt "Is this a gaming machine?[y/n]"
+if ( $reply -match "[yY]" ) { 
+  cinst -y nvidia-display-driver
+  cinst -y steam
+}
+
+# Gaming Laptop
+$reply = Read-Host -Prompt "Is this a Razer?[y/n]"
+if ( $reply -match "[yY]" ) { 
+  cinst -y razer-synapse-2
+}
+
+
+<#
+	$reply = Read-Host -Prompt "Continue?[y/n]"
+	if ( $reply -match "[yY]" ) { 
+    # Highway to the danger zone 
+	}
+#>
