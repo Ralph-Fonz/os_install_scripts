1b8746c9f5b47b110d4b6551d338708fecdcb7a7
Ralph_Fonz <raphael@fonseca.tech>
2021-02-22 10:25:52 -0500

    updating script

--- Win-10/chocolateyPackages/Win10VMChoco.ps1
+++ Win-10/chocolateyPackages/Win10VMChoco.ps1
@@ -3,7 +3,8 @@
 # @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 
 # https://http://chocolatey.org/packages/<name>
-
+# allow scripts to run > open powershell as admin 
+# set-executionpolicy remotesigned
 
 ######### General PC computing ###############
 cinst -y chromium
